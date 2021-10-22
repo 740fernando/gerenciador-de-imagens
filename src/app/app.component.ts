98f070272424cc581a68f3d0b2ec3fb689955cc2
fsouviei <fernando.souzavieira@everis.nttdata.com>
2021-10-22 13:27:58 -0300

    Criacao do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 }
+
+
+//Se abrirmos app.component.html, encontraremos o código referente à apresentação que vemos na página do navegador.Então, o Angular carregará, exibindo o primeiro componente, e seu template.Voltando a app.component.ts, o styleUrls: ['./app.component.css'] informa onde se localiza o CSS utilizado por este componente.
