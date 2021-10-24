ab2ed26382ce102a9989a2c65a5e68a63973b515
fsouviei <fernando.souzavieira@everis.nttdata.com>
2021-10-24 14:09:39 -0300

    Implementando a logica do Grid

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photoservice';
 
 @Component({
-  selector: 'app-photo-list',
+  selector: 'ep-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
 export class PhotoListComponent implements OnInit {
 
-  photos: any[] = [];
+  photos: Photo[] = [];
 
   constructor(private photoService: PhotoService,
     private activatedRoute: ActivatedRoute
