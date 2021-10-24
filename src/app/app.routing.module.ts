242c93cfe72f0faf50b1b67b5933ad4c046e167d
fsouviei <fernando.souzavieira@everis.nttdata.com>
2021-10-24 13:26:46 -0300

    Segmentos de rotas

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -6,7 +6,7 @@ import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 
 const routes: Routes = [
-  { path: 'user/flavio', component: PhotoListComponent },
+  { path: 'user/:userName', component: PhotoListComponent },
   { path: 'p/add', component: PhotoFormComponent },
   { path: '**', component: NotFoundComponent }
 ];
@@ -17,3 +17,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
+
+//O Angular sim interpreta a URL e verifica se há um roteamento associado. Se não tiver nenhum roteamento que serve, é mostrado um erro no console.
